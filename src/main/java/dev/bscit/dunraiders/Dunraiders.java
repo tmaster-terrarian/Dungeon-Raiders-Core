047805162bbc3a839b1ae46b0e073e5c67645bdb
bscit <bscitdv@gmail.com>
2025-09-30 12:56:26 -0700

    init

---
+++ src/main/java/dev/bscit/dunraiders/Dunraiders.java
@@ -0,0 +1,10 @@
+package dev.bscit.dunraiders;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Dunraiders implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
